7e500f1fb03e847a2d66830151af3c97d5bcd038
Josh Mabry <mabry1985@gmail.com>
2021-02-09 13:13:14 -0800

    initial commit, skeleton out sidedrawer component

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from '@stencil/core/internal';
+
+@Component({
+  tag: 'jm-side-drawer',
+})
+export class SideDrawer {
+  render() {
+    return (
+      <div>
+        <h1>The Side Drawer</h1>
+      </div>
+    );
+  }
+}
+
