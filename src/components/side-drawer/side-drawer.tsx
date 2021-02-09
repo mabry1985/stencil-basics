84973e568d4efdbd8bb1f69a7d3255e1bd1e4093
Josh Mabry <mabry1985@gmail.com>
2021-02-09 15:06:39 -0800

    add state and conditional rendering for nav and contact tabs

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core/internal';
+import { Component, h, Prop, State } from '@stencil/core/internal';
 
 @Component({
   tag: 'jm-side-drawer',
@@ -6,14 +6,38 @@ import { Component, h, Prop } from '@stencil/core/internal';
   shadow: true,
 })
 export class SideDrawer {
+  @State() showContactInfo = false;
+
   @Prop({ reflect: true }) title: string;
   @Prop({ reflect: true, mutable: true }) open: boolean;
 
   onCloseDrawer = () => {
     this.open = false;
-  }
+  };
+
+  onContentChange = (content: string) => {
+    this.showContactInfo = content === 'contact';
+  };
 
   render() {
+    let mainContent = <slot />;
+
+    if (this.showContactInfo) {
+      mainContent = (
+        <div id="contact-information">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email</p>
+          <ul>
+            <li>Phone: 888888888</li>
+            <li>
+              Email:
+              <a href="mailto:something@something.com">something@something.com</a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
     return (
       <aside>
         <header>
@@ -21,12 +45,12 @@ export class SideDrawer {
           <button onClick={this.onCloseDrawer}>X</button>
         </header>
         <section id="tabs">
-          <button class="active" >Navigation</button>
-          <button>Contact</button>
+          <button class={!this.showContactInfo ? 'active' : ''} onClick={() => this.onContentChange('nav')}>
+            Navigation
+          </button>
+          <button  class={this.showContactInfo ? 'active' : ''} onClick={() => this.onContentChange('contact')}>Contact</button>
         </section>
-        <main>
-          <slot />
-        </main>
+        <main>{mainContent}</main>
       </aside>
     );
   }
